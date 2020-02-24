9919fdfe174104ffe8ff2e89f70b3ae747050ed6
huangmei123 <1593702111@qq.com>
2020-02-24 23:13:44 +0800

    iview form

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import iview from 'iview';  //UI框架 
+import 'iview/dist/styles/iview.css';
+
+Vue.use(iview); /* 嫁接 */
 
 Vue.config.productionTip = false
 
